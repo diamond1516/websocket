caf4fd87d4597b9d56368ffd2bb8f9335514a1be
user <ahmadboyabdurahimov589@gmail.com>
2023-12-20 19:23:34 +0500

    new changes

--- app/consumers.py
+++ app/consumers.py
@@ -10,7 +10,6 @@ class SomeConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps(
             {'msg': 'salom'}
         ))
-        print(self.scope.get('user'))
 
     async def disconnect(self, close_code):
         pass
