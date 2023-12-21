708e7e730d5b981ccd4e8ed7789294adce7b942e
user <ahmadboyabdurahimov589@gmail.com>
2023-12-21 11:07:39 +0500

    new changes

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from app.models import Model
+
 # Register your models here.
+
+
+admin.site.register(Model)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Model(models.Model):
+    name = models.TextField()
+    title = models.TextField(null=True)
+    son = models.TextField(null=True)
+
