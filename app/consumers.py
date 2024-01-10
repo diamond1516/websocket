5f58f8f8ddfb4284209c82b3425baaa353d3b970
user <ahmadboyabdurahimov589@gmail.com>
2024-01-10 13:51:20 +0500

    new changes

--- app/consumers.py
+++ app/consumers.py
@@ -2,14 +2,23 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
 from channels.consumer import SyncConsumer
+from channels.layers import get_channel_layer
 
 
 class SomeConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.room_group_name = 'salom'
+        self.channel_layer = self.channel_layer or get_channel_layer()
+
+        await self.channel_layer.group_add(
+            self.room_group_name, self.channel_name
+        )
         await self.accept()
-        await self.send(text_data=json.dumps(
-            {'msg': 'salom'}
-        ))
+
+        # channel_layer obyektini saqlang
+        self.scope['channel_layer'] = self.channel_layer
+
+        await self.send(text_data=json.dumps({'msg': 'salom'}))
         print(self.scope.get('user'))
 
     async def disconnect(self, close_code):
@@ -25,3 +34,10 @@ class SomeConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': message
         }))
+
+    async def notification_send(self, event):
+        await self.send(
+            {"action": "new_notification", "data": event["data"]}
+        )
+
+
--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.db import models
+
 
 # Create your models here.
 
@@ -7,4 +10,29 @@ class Model(models.Model):
     name = models.TextField()
     title = models.TextField(null=True)
     son = models.TextField(null=True)
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    status = models.CharField(
+        max_length=255,
+        choices=[('pending', 'Pending'), ('cencelled', 'Cencelled'), ('completed', 'Completed')]
+    )
+
+    def save(self, *args, **kwargs):
+        channel_layer = self.scope.get('channel_layer', get_channel_layer())
+        async_to_sync(channel_layer.group_send)(
+            'salom',
+            {
+                "type": "notification.send",
+                "data": {
+                    'status': self.status,
+                    "message": f"Buyurtmangiz {self.status} holatiga kirdi",
+                    "order": self.pk,
+                },
+            },
+        )
+
+        super(Order, self).save(*args, **kwargs)
+
 
